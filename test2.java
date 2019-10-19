66ca91d407e76704b15e46dfc9d95c9fe2dcf8b0
Michael Lynn <hailviral101@yahoo.com>
2019-10-19 05:19:55 -0600

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,16 @@
+package School;
+
+
+class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+		Printer myPrinter = new Printer();
+		myPrinter.myprint();
+        System.out.println("Hello, World!"); 
+
+    }
+
+
+}
---
+++ test3.java
@@ -0,0 +1,19 @@
+package School;
+
+class Printer {
+
+public void myprint(){
+
+	System.out.println("myprint"); 
+
+}
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, Printer!"); 
+
+    }
+
+}
